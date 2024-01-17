4d8ca5334b77153815b0f07c817334330a5ca739
artisenzubair <124689454+artisenzubair@users.noreply.github.com>
2024-01-17 14:02:27 +0530

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,40 +1,25 @@
-// shared-library/vars/mavenBuild.groovy
+// vars/gitCheckoutStage.groovy
 
 def call() {
     pipeline {
-        agent any
-        tools {
-            maven 'maven'
-        }
+        agent none
+
         stages {
-            stage('gitcheckout') {
+            stage('Git Checkout') {
+                agent {
+                    label "maven"
+                }
                 steps {
                     script {
-                        // Use the 'node' block to run on a specific agent
-                        node {
-                            // Inside 'node', you can use 'git'
-                            git 'https://github.com/artisenzubair/my-app.git'
-                        }
+                        // Inside 'script', you can use 'git'
+                        git 'https://github.com/artisenzubair/my-app.git'
                     }
                 }
             }
-            stage('Maven Build') {
-                steps {
-                    script {
-                        // Run Maven with 'clean install'
-                        sh 'mvn clean install'
-                    }
-                }
-            }
+
+            // Add more stages as needed
         }
 
-        post {
-            success {
-                echo 'Maven build successful!'
-            }
-            failure {
-                echo 'Maven build failed!'
-            }
-        }
+        // Add post section if required
     }
 }
